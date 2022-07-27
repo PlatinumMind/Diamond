ba31156bcba9072d96f3e59cbe8138cfc08cf735
Fira <frodggyxd@gmail.com>
2022-07-26 22:00:04 -0400

    Revert "Revert "Revert "added readme, safe exec, install script"""
    This reverts commit d55ed8f7e80bdc9cba0be25a687314e88fa89783.

--- src/main.go
+++ src/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"regexp"
-	"runtime"
-	"strings"
 
 	"github.com/pelletier/go-toml/v2"
+	node "github.com/tidwall/go-node"
 )
 
 type Step struct {
@@ -26,7 +22,6 @@ type Clean struct {
 }
 
 type Recipient struct {
-	Vars  map[string]string
 	Pre   Pre
 	Step  []Step
 	Clean Clean
@@ -39,77 +34,36 @@ func main() {
 		panic(err)
 	}
 	toml.Unmarshal(content, &cfg)
-	fmt.Printf("%v\n", cfg.Vars["test1"])
 	run(cfg)
 }
 
 func run(cfg Recipient) {
-
+	vm := node.New(nil)
 	if cfg.Pre.Cmd != "" {
-		println("preparing build: ")
-		cmd := Vars(cfg.Pre.Cmd, cfg.Vars)
-		cmd = strings.ReplaceAll(cmd, "'", "\\'")
-		cmd = strings.ReplaceAll(cmd, "\"", "\\\"")
-		if runtime.GOOS == "windows" {
-			o, err := exec.Command("cmd", "/c", cmd).Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			println(string(o))
-		} else {
-			o, err := exec.Command("sh", "-c", cmd).Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			println(string(o))
+		println("preparing build")
+		
+		v := vm.Run("child_process.execSync('"+cfg.Pre.Cmd+"')")
+		if err := v.Error(); err != nil {
+			log.Fatal(err)
 		}
+		println(v.String())
 	}
 	for _, step := range cfg.Step {
 		print("[" + step.Name + "] started running\n")
-		cmd := Vars(step.Cmd, cfg.Vars)
-		cmd = strings.ReplaceAll(cmd, "'", "\\'")
-		cmd = strings.ReplaceAll(cmd, "\"", "\\\"")
-		if runtime.GOOS == "windows" {
-			o, err := exec.Command("cmd", "/c", cmd).Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			println(string(o))
-		} else {
-			o, err := exec.Command("sh", "-c", cmd).Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			println(string(o))
+		v := vm.Run("child_process.execSync('"+step.Cmd+"')")
+		if err := v.Error(); err != nil {
+			log.Fatal(err)
 		}
+		println(v.String())
 		println("[" + step.Name + "] finished running\n")
 	}
 
 	if cfg.Clean.Cmd != "" {
 		println("cleaning build:")
-		cmd := Vars(cfg.Clean.Cmd, cfg.Vars)
-		cmd = strings.ReplaceAll(cmd, "'", "\\'")
-		cmd = strings.ReplaceAll(cmd, "\"", "\\\"")
-		if runtime.GOOS == "windows" {
-			o, err := exec.Command("cmd", "/c", cmd).Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			println(string(o))
-		} else {
-			o, err := exec.Command("sh", "-c", cmd).Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			println(string(o))
+		v := vm.Run("child_process.execSync('"+cfg.Pre.Cmd+"')")
+		if err := v.Error(); err != nil {
+			log.Fatal(err)
 		}
+		println(v.String())
 	}
-}
-
-func Vars(input string, data map[string]string) string {
-	m1 := regexp.MustCompile(`{(.*?)}`)
-	m2 := regexp.MustCompile(`{|}`)
-	return m1.ReplaceAllStringFunc(input, func(s string) string {
-		return data[m2.Split(s, 10)[1]]
-	})
 }
