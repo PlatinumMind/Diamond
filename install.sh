bea7c80ff438dbee77681a304a557a5b17d502bd
Arif <75814033+frodggy@users.noreply.github.com>
2022-08-14 13:37:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "downloading binarie from github"
-wget -qo- "https://github.com/xonon-business/Diamond/releases/download/v0.0.1/dbuild-amd64-linux" | bash
+wget -qo- "https://github.com/xonon-business/Diamond/releases/download/v0.0.2/dbuild-amd64-linux" | bash
 echo "installing binary to $HOME/.local/bin/"
 mv dbuild-amd64-linux $HOME/.local/bin/dbuild
 
